da728a54c9d8255357610279bc3e82d77f0f715d
Jon <jmrapp1270@gmail.com>
2017-04-18 11:16:57 -0400

    Removed spells; old and irrelevant

--- Spells/src/com/baseball435/spells/abstr/AbstractHitSpell.java
+++
@@ -1,9 +0,0 @@
-package com.baseball435.spells.abstr;
-
-public abstract class AbstractHitSpell extends AbstractDamageSpell {
-
-	public AbstractHitSpell(String name, int price, AbstractSpell parentSpell, String className, int itemId, int manaCost, int coolDownTime, double damage) {
-		super(name, price, parentSpell, className, itemId, manaCost, coolDownTime, damage);
-	}
-
-}
